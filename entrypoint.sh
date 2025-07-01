84424c7b58b231eba0e6a27784aca62b11df60c0
syuu2449 <syuu2449@gmail.com>
2025-07-01 18:31:18 +0900

    ・vite_railsに必要なgemを追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ sudo rm -f /tmp/pids/server.pid
 
 # 権限をホスト（実際は実行ユーザー）に合わせる
 sudo chown -R appuser:appgroup /app
+sudo chown -R appuser:appgroup /usr/local/bundle/
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
