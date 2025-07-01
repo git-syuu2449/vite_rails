9897d64e69bcfe5d9438bb79b3e54ab9aada174c
syuu2449 <syuu2449@gmail.com>
2025-07-02 00:02:39 +0900

    ・viteとvue,tailwindcssの連携 　・各種設定ファイルを修正 　・サンプル用のページ作成 　・READMEの整備

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,9 +1,24 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  
+    content: [
+    './app/views/**/*.{html,erb,haml,slim}',                // Rails のビューファイル
+    './app/helpers/**/*.rb',                                // helper 内でHTMLタグを返すことがある場合
+    './app/frontend/js/**/*.{js,ts,jsx,tsx,vue}',           // Viteで読み込むフロントエンドコード
+    './app/frontend/css/**/*.{css,sass}',                   // Viteで読み込むフロントエンドコード
+    './app/components/**/*.{erb,html,rb}',                  // ViewComponent
+  ],
+  
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+            sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        },
+    },
   },
-  plugins: [],
-}
 
+  plugins: [forms],
+}
